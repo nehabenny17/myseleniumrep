6bd6e1f9ddfdce5215d2937aeeea304bc9fbc507
nehabenny17 <nehabenrocks@gmail.com>
2024-06-28 10:18:13 +0530

    PUSH

---
+++ src/arraysstringsloopsfunctions/operators.java
@@ -0,0 +1,17 @@
+package arraysstringsloopsfunctions;
+
+public class operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//these operators are used:// < >  <= >= != ==
+		//    %-used for getting reminder
+ int a=45;
+ int b=7;
+ if(a!=b) {
+	 System.out.println("not equal");
+ }
+ System.out.println(a%b);
+	}
+
+}
